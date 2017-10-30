8634787146470ed2cebefa289bb4f484915e56df
Swapna Yenne <swapna.yenne@gmail.com>
2017-10-30 15:37:01 +0000

    5Day_WeatherForecastAndroidApp

---
+++ app/src/androidTest/java/com/swapna/weatherforecast/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.swapna.weatherforecast;
+
+/**
+ * Created by swapna on 30/10/2017.
+ */
+
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/weatherforcast/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package weatherforcast;
+
+/**
+ * Created by swapna on 30/10/2017.
+ */
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
